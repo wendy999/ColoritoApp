4f738d916b981b6d2f449e048fe78cd7e969e0ff
Sena <wendyvanessa.ma@gmail.com>
2018-07-27 17:46:00 -0500

    DBColorito culminada

--- app/src/main/java/com/platzi/coloritoapp/Utilidades/DBColorito.java
+++ app/src/main/java/com/platzi/coloritoapp/Utilidades/DBColorito.java
@@ -1,8 +1,12 @@
 package com.platzi.coloritoapp.Utilidades;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class DBColorito extends SQLiteOpenHelper
 {
@@ -28,4 +32,60 @@ public class DBColorito extends SQLiteOpenHelper
         super(context,BD_ATOS,null,1);
     }
 
+    SQLiteDatabase db;
+
+    public void openBD(boolean write)
+    {
+        if (write)
+        {
+            this.getWritableDatabase();
+        }
+        else
+        {
+            this.getWritableDatabase();
+        }
+    }
+
+    public void closDB()
+    {
+        db.close();
+    }
+
+    public long insertScore(int score, int scoreError)
+    {
+        ContentValues insertt = new ContentValues();
+        insertt.put(PUNTAJE,score);
+        insertt.put(PUNTAJE_INCORRECTO,scoreError);
+
+        long insert = db.insert(TABLA_COLORITO,null,insertt);
+        closDB();
+        return insert;
+    }
+
+
+    public ArrayList<String> listScore()
+    {
+        ArrayList<String> Scores = new ArrayList<String>();
+
+        int Score;
+        int ScoreError;
+
+        String query = "SELECT * FROM"+BD_ATOS+" ORDER BY "+ PUNTAJE+" DESC LIMIT 4 ";
+        Cursor cursor = db.rawQuery(query,null);
+
+        if (cursor.moveToFirst() && cursor !=null)
+        {
+            int contador = 0;
+            do {
+                contador++;
+                Score = cursor.getInt(1);
+                ScoreError = cursor.getInt(2);
+                Scores.add("CORRECTOS: " + Score + " INCORRECTOS: " + ScoreError);
+            }
+            while (cursor.moveToNext());
+        }
+        closDB();
+        cursor.close();
+        return Scores;
+    }
 }
