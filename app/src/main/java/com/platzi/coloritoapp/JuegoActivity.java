1c655b5c415badb705c80adef5e46e206268dc2a
Sena <wendyvanessa.ma@gmail.com>
2018-08-03 18:42:44 -0500

    creación provisional del diseño de juegoActivity

--- app/src/main/java/com/platzi/coloritoapp/JuegoActivity.java
+++ app/src/main/java/com/platzi/coloritoapp/JuegoActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class JuegoActivity extends AppCompatActivity {
 
+    String [] arregloPalabras,arregloColores;
+
+    private String [] MWords = {"Amarillo","Aul","Verde","Rojo"};
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
+
+
     }
 }
